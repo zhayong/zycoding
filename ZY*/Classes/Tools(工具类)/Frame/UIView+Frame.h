d2e9f1ccfe226bac568bbc77108340ea2fdd65cc
Zhayong <Zhayong@zhayong-macbook-pro.local>
2016-03-14 12:32:22 +0800

    add UIView + Frame

---
+++ ZY*/Classes/Tools(工具类)/Frame/UIView+Frame.h
@@ -0,0 +1,20 @@
+//
+//  UIView+Frame.h
+//  ZY*
+//
+//  Created by Zhayong on 3/14/16.
+//  Copyright © 2016 查勇的小仓库. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGPoint origin;
+@property (nonatomic, assign) CGSize size;
+
+@end
